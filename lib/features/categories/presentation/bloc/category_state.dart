d9668a70c1cb2229e6dac5c1ebeeadbaff20dd8e
Igor Sapaev <igors@MacBook-Pro-Igor.local>
2025-06-15 12:37:57 +0300

    Add Clean Architecture structure

--- lib/features/categories/presentation/bloc/category_state.dart
+++ lib/features/categories/presentation/bloc/category_state.dart
@@ -1,10 +1,10 @@
-part of "category_bloc.dart";
+part of 'category_bloc.dart';
 
 abstract class CategoryState extends Equatable {
   const CategoryState();
 
   @override
-List <Object> get props => [];
+  List<Object> get props => [];
 }
 
 class CategoryInitial extends CategoryState {}
@@ -12,7 +12,7 @@ class CategoryInitial extends CategoryState {}
 class CategoryLoading extends CategoryState {}
 
 class CategoryLoaded extends CategoryState {
-  final List <Category> categories;
+  final List<Category> categories;
 
   const CategoryLoaded({required this.categories});
 
@@ -27,7 +27,4 @@ class CategoryError extends CategoryState {
 
   @override
   List<Object> get props => [message];
-
 }
-
-
